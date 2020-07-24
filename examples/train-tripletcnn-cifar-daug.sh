eab1bbfa7a4a1c1d779266d66d7cb7f1313b3500
verrannt <paschroeder@uni-osnabrueck.de>
2020-07-24 18:14:42 +0200

    add script for training TripletCNN with triplet loss

--- examples/train-tripletcnn-cifar-daug.sh
+++ examples/train-tripletcnn-cifar-daug.sh
@@ -13,8 +13,13 @@ python train.py \
 --data_file ~/Storage/Datasets/cifar10/hdf5/cifar10.hdf5 \
 --group_tr train \
 --group_val test \
---train_dir ./log/cifar10/tripletcnn/noreg/bn/inv/r24/ \
+--train_dir ./log/cifar10/tripletcnn/noreg/bn/inv/r31/ \
 --daug_params heavier.yml \
 --train_config_file config_train/cifar/tripletcnn/noreg_bn.yml \
+--aug_per_img_tr 16 \
+--daug_invariance_params loss1_exp.yml \
+--class_invariance_params noinv.yml \
 --save_model_every 10 \
---no_fit_generator
+--no_fit_generator \
+--resume_training ./log/cifar10/tripletcnn/noreg/bn/inv/r31/model_*_030 \
+--triplet_loss 
